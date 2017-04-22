179726f429f60973e6188be8daa9e8c4ebe8f9ce
Geoff P <geoffperrin@gmail.com>
2017-04-22 13:33:00 -0400

    added TLC query and made mta query bash 3 compatible

--- 01_setup.sh
+++ 01_setup.sh
@@ -10,18 +10,33 @@ cd $REPOLOC
 # if it doesn't exist, create the data director
 mkdir -p data
 
+# download and prepare the data                                                                            
+# mta turnstile data                                                                                       
+# loop through and download   
+
+# check bash version (bash v3 handles dates different than bash v4)
+if [ ${BASH_VERSION::1} -eq 3 ]; then
+    d=$(date -j -f %y%m%d 161231 +%y%m%d)
+    for i in $(seq 7 7 28)
+    do
+        curl -o ./data/mta_$d.csv http://web.mta.info/developers/data/nyct/turnstile/turnstile_$d.txt
+        d=$(date -j -f %y%m%d -v -$i'd' 161231 +%y%m%d)
+        #echo $d
+    done
+fi
+
 # download and prepare the data
 
 # mta turnstile data
 
 # loop through and download
-d=$(date +"%y%m%d" -d "last saturday")
-for i in {7..28..7}
-do
-    curl -o ./data/mta_$d.csv http://web.mta.info/developers/data/nyct/turnstile/turnstile_$d.txt
-    d=$(date +%y%m%d -d "$d - $i days")
-done
-
+if [ ${BASH_VERSION::1} -eq 4 ]; then
+    for i in {7..28..7}
+    do
+        curl -o ./data/mta_$d.csv http://web.mta.info/developers/data/nyct/turnstile/turnstile_$d.txt
+        d=$(date +%y%m%d -d "$d - $i days")
+    done
+fi
 # combine mta files into one file
 count=0 # counter so we only get the column headers once
 # loop through files
@@ -36,4 +51,14 @@ do
     count=$(($count + 1)) # increase the count
     # and remove the small csv
     rm ./data/$wk
+done
+
+
+# next query TLC data for 2016 january to march
+# loop through and download                                                                               
+for num in $(seq -w 01 03)
+do
+    echo $num
+    curl -o ./data/tlc_yellow_2016_$num.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2016\
+-0$num.csv
 done
